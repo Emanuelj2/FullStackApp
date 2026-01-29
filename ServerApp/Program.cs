8ebb522415b5751f6b63756d7551ce6847613ef7
Emanuel Jose <emanueljose0522@hotmail.com>
2026-01-28 20:00:42 -0500

    this is the initial commit of the full stack project

---
+++ ServerApp/Program.cs
@@ -0,0 +1,43 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//add CORS
+/*
+    What is CORS?
+    -CORS stands for Cross-Origin Resource Scharring
+    -CORS lets your front end safely talk to your back end when they run on different ports or domains.
+    -protects users from cross-site attacks
+
+    -in a real application we would NOT use AllowAnyOrgin() insted we would use WithOrgins("https://myfrontend.com") 
+*/
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazor",
+    policy =>
+    {
+        policy
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowBlazor");
+
+app.MapGet("/api/products", () =>
+{
+
+    return new[]
+
+    {
+
+        new { Id = 1, Name = "Laptop", Price = 1200.50, Stock = 25 },
+
+        new { Id = 2, Name = "Headphones", Price = 50.00, Stock = 100 }
+
+    };
+
+});
+
+app.Run();
